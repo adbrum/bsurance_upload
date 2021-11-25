924b044c8fdf826fea240f80cfd1992f01940c5f
adbrum <adbrum@outlook.com>
2021-11-25 19:29:19 +0000

    Created model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=500)
+    image_file = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

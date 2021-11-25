568ef3649d167f112ff93721857a29e9de35de11
adbrum <adbrum@outlook.com>
2021-11-25 20:48:55 +0000

    Added another URL to image

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from core import views
 urlpatterns = [
     path('', views.upload_image, name='upload_image'),
     path('img/<str:image_name>/', views.show_images, name='show_image'),
+    path('image/<str:image_name>/', views.show_images, name='image'),
 ]
